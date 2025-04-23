7836ccd993f704f28e1f8340b632e209c699a094
JoostCroonen <joost.croonen@kuleuven.be>
2025-04-23 13:48:10 +0200

    Add project files.

---
+++ TestSetup.cpp
@@ -0,0 +1,12 @@
+﻿// TestSetup.cpp : Defines the entry point for the application.
+//
+
+#include "TestSetup.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake. " << endl;
+	return 0;
+}
